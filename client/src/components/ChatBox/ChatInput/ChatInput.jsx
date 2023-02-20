a3c233fc539e46a36f13486fd4aaaddf1e26bb16
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2023-02-19 20:13:35 -0500

    Implement chat box

---
+++ client/src/components/ChatBox/ChatInput/ChatInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ChatInput = () => {
+  return (
+    <div className="chat-input-holder">
+        <textarea className="chat-input-textarea" placeholder="Type your message here..." rows="1">
+        </textarea>
+    </div>
+  )
+}
+
+export default ChatInput
