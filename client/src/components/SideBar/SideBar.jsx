76d6dd651365c6fe608835af81715b7f3164fc62
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2023-02-19 20:06:43 -0500

    Implement sidebar

---
+++ client/src/components/SideBar/SideBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <aside className="side-menu">
+        <div className="side-menu-btn">
+            {/* TODO: ADD FONT AWESOME + */}
+            <span>+</span>
+            New Chat
+        </div>
+    </aside>
+  )
+}
+
+export default SideBar
